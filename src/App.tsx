2937d0962e14162e1ee9847dc6138d6f7f471ea4
Kamrul Saad <skamrul2680@gmail.com>
2023-11-12 19:09:40 +0600

    Added necessary packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import styled from "styled-components";
+import bg from "./assets/bg.png";
+
 function App() {
-  return <></>;
+  return (
+    <AppStyled bg={bg}>
+      {/* {orbMemo}
+  <MainLayout>
+    <Navigation active={active} setActive={setActive} />
+    <main>
+      {displayData()}
+    </main>
+  </MainLayout> */}
+    </AppStyled>
+  );
 }
+
+const AppStyled = styled.div<{ bg: string }>`
+  height: 100vh;
+  background-image: url(${(props) => props.bg});
+  position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
+`;
 
 export default App;
